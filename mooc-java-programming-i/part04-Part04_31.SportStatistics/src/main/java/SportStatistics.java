48f4bb58625736dace2b60725984139856ec8147
larry charbonneau <larrycharbonneau4@gmail.com>
2020-08-26 09:54:28 -0400

    sync

--- mooc-java-programming-i/part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
+++ mooc-java-programming-i/part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
@@ -7,6 +7,18 @@ public class SportStatistics {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("File:");
+        String file = scan.nextLine();
+        
+        try (Scanner reader = new Scanner(Paths.get(file))) {
+            ArrayList<Teams> teams = new ArrayList<>();
+            while (reader.hasNextLine()) {
+                
+            }
+            
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
 
     }
 
