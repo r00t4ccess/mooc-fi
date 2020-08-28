8fa4ff5554506e5ffba6f50dd332ee06bfa37ddd
larry charbonneau <larrycharbonneau4@gmail.com>
2020-08-27 21:05:47 -0400

    sync

--- mooc-java-programming-i/part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
+++ mooc-java-programming-i/part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
@@ -9,12 +9,34 @@ public class SportStatistics {
         Scanner scan = new Scanner(System.in);
         System.out.println("File:");
         String file = scan.nextLine();
+        System.out.println("Team:");
+        String team = scan.nextLine();
+        //team = team.toUpperCase();
+        int wins = 0;
+        int losses = 0;
+        int gamesPlayed = 0;
         
         try (Scanner reader = new Scanner(Paths.get(file))) {
-            ArrayList<Teams> teams = new ArrayList<>();
+            ArrayList<String> games = new ArrayList<>();
             while (reader.hasNextLine()) {
-                
+                games.add(reader.nextLine());
             }
+            for (String i : games) {
+                String[] parts = i.split(",");
+                if (parts[0].equals(team) || parts[1].equals(team)) {
+                    gamesPlayed++;
+                    if (parts[0].equals(team) && Integer.valueOf(parts[2]) > Integer.valueOf(parts[3])) {
+                        wins++;
+                    } else if (parts[1].equals(team) && Integer.valueOf(parts[2]) < Integer.valueOf(parts[3])) {
+                        wins++;
+                    } else {
+                        losses++;
+                    }
+                }
+            }
+            System.out.println("Games: " + gamesPlayed);
+            System.out.println("Wins: " + wins);
+            System.out.println("Losses: " + losses);
             
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
