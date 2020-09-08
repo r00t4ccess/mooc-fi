f03194ac898dcfd28a469150fdd8026b357508eb
Larry Charbonneau <lawrence.charbonneau@vw.com>
2020-09-08 16:12:17 -0400

    sync

--- mooc-java-programming-i/part06-Part06_07.HeightOrder/src/main/java/Room.java
+++ mooc-java-programming-i/part06-Part06_07.HeightOrder/src/main/java/Room.java
@@ -8,6 +8,49 @@
  *
  * @author charbol
  */
+
+import java.util.ArrayList;
+
 public class Room {
+    private ArrayList<Person> members = new ArrayList<>();
+    
+    public Room() {
+        
+    }
+    
+    public void add(Person person) {
+        this.members.add(person);
+    }
+    
+    public boolean isEmpty() {
+        return this.members.isEmpty();
+    }
+    
+    public ArrayList<Person> getPersons() {
+        
+        return this.members;
+    }
+    
+    public Person shortest() {
+        if(this.members.isEmpty()) {
+            return null;
+        }
+        Person shortest = this.members.get(0);
+        for (Person i : this.members) {
+            if (shortest.getHeight() > i.getHeight()) {
+                shortest = i;
+            }
+        }
+        return shortest ;
+    }
+    
+    public Person take() {
+        if (this.members.isEmpty()) {
+            return null;
+        }
+        Person shortest = shortest();
+        this.members.remove(shortest);
+        return shortest;
+    }
     
 }
