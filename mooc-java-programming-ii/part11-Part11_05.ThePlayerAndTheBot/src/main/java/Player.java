11654c9750ae5563c3695058e7229f3a561fc819
Larry Charbonneau <lawrence.charbonneau@vw.com>
2020-10-15 15:51:05 -0400

    sync

---
+++ mooc-java-programming-ii/part11-Part11_05.ThePlayerAndTheBot/src/main/java/Player.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author charbol
+ */
+public class Player {
+    private String name;
+    
+    public Player() {
+        
+    }
+    
+    public void play(){
+        
+    }
+    
+    public void printName() {
+        
+    }
+}
---
+++ mooc-java-programming-ii/part11-Part11_06.SaveablePerson/src/main/java/Person.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author charbol
+ */
+public class Person implements Saveable{
+    private String name;
+    private String address;
+    
+    public Person() {
+        
+    }
+    
+    @Override
+    public void save(){
+        
+    }
+    
+    @Override
+    public void delete() {
+        
+    }
+    
+    @Override
+    public void load(String address) {
+        
+    }
+    
+    
+}
