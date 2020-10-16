3f5e3d4a17045b8bdc0ae98c7ca7220a54d70498
larry charbonneau <larrycharbonneau4@gmail.com>
2020-10-16 07:55:40 -0400

    sync

--- mooc-java-programming-ii/part11-Part11_10.FlightControl/src/main/java/FlightControl/Main.java
+++ mooc-java-programming-ii/part11-Part11_10.FlightControl/src/main/java/FlightControl/Main.java
@@ -1,9 +1,17 @@
 package FlightControl;
 
+import FlightControl.UI.*;
+import FlightControl.logic.FlightControl;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        // Write the main program here. It is useful to create some classes of your own.
+        Scanner scan = new Scanner(System.in);
+        FlightControl flightc = new FlightControl();
+        
+        
+        TextUI ui = new TextUI(flightc, scan);
+        ui.start();
     }
 }
